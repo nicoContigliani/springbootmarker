a932d286198284eb914395a7edd68e7c92b1aaf2
nicoContigliani <nico.contigliani@gmail.com>
2021-04-11 11:03:16 -0300

    first commit

---
+++ primero/src/test/java/com/primero/primero/PrimeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primero.primero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
